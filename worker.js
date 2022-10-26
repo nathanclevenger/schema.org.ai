d89fbb86229e7cfe5aa0aa86621698fce07a4d94
Nathan Clevenger <nateclev@gmail.com>
2022-10-25 20:40:12 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ import { withParams, json } from 'https://pkg.do/itty-router-extras'
 
 const router = Router()
 
-router.get('/', ({url}) => json({ url, 'Deploy in Seconds 🚀': 'https://github.com/drivly/itty-router-template/generate' }))
+router.get('/', req => fetch('https://schema.org/version/latest/schemaorg-current-https.jsonld'))
 router.get('/:resource?/:id?', withParams, ({url, resource, id}) => json({ url, resource, id}))
 
 export default {
